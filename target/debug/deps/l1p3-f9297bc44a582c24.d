68d37e923f783fbb5822b4fd8621d9d64ef8a11b
novikov.andrey70 <novikov.andrey70@wb.ru>
2024-09-03 01:38:28 +0500

    feat: l1p3

---
+++ target/debug/deps/l1p3-f9297bc44a582c24.d
@@ -0,0 +1,5 @@
+/Users/andreynovikov/Desktop/rust/wbl1/l1p3/target/debug/deps/l1p3-f9297bc44a582c24: src/main.rs
+
+/Users/andreynovikov/Desktop/rust/wbl1/l1p3/target/debug/deps/l1p3-f9297bc44a582c24.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/l1p3.d
@@ -0,0 +1,1 @@
+/Users/andreynovikov/Desktop/rust/wbl1/l1p3/target/debug/l1p3: /Users/andreynovikov/Desktop/rust/wbl1/l1p3/src/main.rs
